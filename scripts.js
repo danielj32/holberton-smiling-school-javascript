678f9059935d4cab7ccb6495b02ea3246fb04627
danielj32 <danieljuanito21@hotmail.com>
2021-04-20 23:03:02 -0500

    fix add

--- scripts.js
+++ scripts.js
@@ -1,28 +1,28 @@
 $(document).ready(function () {
 
-    // Home - quotes
-    $(function quotes() {
-      let $quotes = $("#quotes");
+    // Home - quot
+    $(function quot() {
+      let $quot = $("#quot");
       $.ajax({
         type: "GET",
-        url: "https://smileschool-api.hbtn.info/quotes",
-        success: function (quotes) {
-          $.each(quotes, function (i, quotes) {
+        url: "https://smileschool-api.hbtn.info/quot",
+        success: function (quot) {
+          $.each(quot, function (i, quot) {
             let a;
-            quotes.id === 2 ? a = "" : a = "active";
+            quot.id === 2 ? a = "" : a = "active";
   
-            $("#quotes").append(
+            $("#quot").append(
               `<div class="carousel-item ${a}">
                 <div class="container-fluid">
                   <div class="row">
                     <div class="col-md-3 col-sm-4 d-flex justify-content-center">
-                      <img class="rounded-circle mb-3" src="${quotes.pic_url}" width="150" height="150">
+                      <img class="rounded-circle mb-3" src="${quot.pic_url}" width="150" height="150">
                     </div>
                     <div class="col-md-9 col-sm-8">
                       <blockquote class="blockquote">
-                        <p class="text-white mb-4">${quotes.text}</p>
-                        <p class="font-weight-bold text-white mb-0">${quotes.name}</p>
-                        <p class="text-white font-italic font-weight-light ">${quotes.title}</p>
+                        <p class="text-white mb-4">${quot.text}</p>
+                        <p class="font-weight-bold text-white mb-0">${quot.name}</p>
+                        <p class="text-white font-italic font-weight-light ">${quot.title}</p>
                       </blockquote>
                     </div>
                   </div>
@@ -35,27 +35,27 @@ $(document).ready(function () {
     });
   
     // Home - popular tutorials
-    $(function popularTutorials() {
-      let $popularTutorials = $("#popularTutorials");
+    $(function TutorialVideo() {
+      let $TutorialVideo = $("#TutorialVideo");
       $.ajax({
         type: "GET",
         url: "https://smileschool-api.hbtn.info/popular-tutorials",
         success: function (data) {
-          $.each(data, function (i, popularTutorials) {
+          $.each(data, function (i, TutorialVideo) {
   
-            $("#popularTutorials").append(`
+            $("#TutorialVideo").append(`
               <div class="col-sm-6 col-md-3 mb-3">
                 <div class="card mb-3 h-100 bg-transparent border-0">
                   <div class="card border-0">
-                    <img class="card-img-top" src="${popularTutorials.thumb_url}" alt="Card image video 1">
+                    <img class="card-img-top" src="${TutorialVideo.thumb_url}" alt="Card image video 1">
                     <img class="card-img-overlay mx-auto my-auto" src="images/play.png" alt="Play icon" width="110">
                   </div>
                   <div class="card-body pl-3 pb-0">
-                    <h6 class="card-title font-weight-bolder">${popularTutorials.title}</h6>
+                    <h6 class="card-title font-weight-bolder">${TutorialVideo.title}</h6>
                     <p class="card-text text-muted"><small></small></p>
                     <div class="d-flex">
-                      <img src="${popularTutorials.author_pic_url}" class="rounded-circle mb-3" width="30" height="30">
-                      <p class="ml-3 text-color font-weight-bold">${popularTutorials.author}</p></div>
+                      <img src="${TutorialVideo.author_pic_url}" class="rounded-circle mb-3" width="30" height="30">
+                      <p class="ml-3 text-color font-weight-bold">${TutorialVideo.author}</p></div>
                     </div>
                     <div class="card-footer border-0 bg-transparent d-flex pl-3">
                       <img src="images/star_on.png" class=" my-auto ml-1" width="15">
@@ -63,7 +63,7 @@ $(document).ready(function () {
                       <img src="images/star_on.png" class=" my-auto ml-1" width="15">
                       <img src="images/star_on.png" class=" my-auto ml-1" width="15">
                       <img src="images/star_off.png" class="my-auto ml-1" width="15">
-                      <p class="ml-auto my-auto text-color"><small class="font-weight-bold">${popularTutorials.duration}</small></p>
+                      <p class="ml-auto my-auto text-color"><small class="font-weight-bold">${TutorialVideo.duration}</small></p>
                     </div>
                   </div>
                 </div>
@@ -75,27 +75,27 @@ $(document).ready(function () {
   
   
     // Home - latest videos
-    $(function latestVideos() {
-      let $latestVideos = $("#latestVideos");
+    $(function lastVideos() {
+      let $lastVideos = $("#lastVideos");
       $.ajax({
         type: "GET",
         url: "https://smileschool-api.hbtn.info/latest-videos",
         success: function (data) {
-          $.each(data, function (i, latestVideos) {
+          $.each(data, function (i, lastVideos) {
   
-            $("#latestVideos").append(`
+            $("#lastVideos").append(`
               <div class="col-sm-6 col-md-3 mb-3">
                 <div class="card mb-3 h-100 bg-transparent border-0">
                   <div class="card border-0">
-                    <img class="card-img-top" src="${latestVideos.thumb_url}" alt="Card image video 1">
+                    <img class="card-img-top" src="${lastVideos.thumb_url}" alt="Card image video 1">
                     <img class="card-img-overlay mx-auto my-auto" src="images/play.png" alt="Play icon" width="110">
                   </div>
                   <div class="card-body pl-3 pb-0">
-                    <h6 class="card-title font-weight-bolder">${latestVideos.title}</h6>
+                    <h6 class="card-title font-weight-bolder">${lastVideos.title}</h6>
                     <p class="card-text text-muted"><small></small></p>
                     <div class="d-flex">
-                      <img src="${latestVideos.author_pic_url}" class="rounded-circle mb-3" width="30" height="30">
-                      <p class="ml-3 text-color font-weight-bold">${latestVideos.author}</p></div>
+                      <img src="${lastVideos.author_pic_url}" class="rounded-circle mb-3" width="30" height="30">
+                      <p class="ml-3 text-color font-weight-bold">${lastVideos.author}</p></div>
                     </div>
                     <div class="card-footer border-0 bg-transparent d-flex pl-3">
                       <img src="images/star_on.png" class=" my-auto ml-1" width="15">
@@ -103,7 +103,7 @@ $(document).ready(function () {
                       <img src="images/star_on.png" class=" my-auto ml-1" width="15">
                       <img src="images/star_on.png" class=" my-auto ml-1" width="15">
                       <img src="images/star_off.png" class="my-auto ml-1" width="15">
-                      <p class="ml-auto my-auto text-color"><small class="font-weight-bold">${latestVideos.duration}</small></p>
+                      <p class="ml-auto my-auto text-color"><small class="font-weight-bold">${lastVideos.duration}</small></p>
                     </div>
                   </div>
                 </div>
